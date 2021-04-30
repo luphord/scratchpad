6cb62a9510e059500739fcf4c404bc3c5c146b4b
luphord <luphord@protonmail.com>
2021-04-30 21:26:25 +0200

    stub and doctests for topological sort

---
+++ stockflow/stockflow.py
@@ -0,0 +1,28 @@
+from typing import Mapping, Iterable, TypeVar
+
+
+T = TypeVar("T")
+
+
+def topological_sort(dag: Mapping[T, Iterable[T]]) -> Iterable[T]:
+    """Perform topological sorting by Kahn's algorithm (see
+    https://en.wikipedia.org/wiki/Topological_sorting#Kahn.27s_algorithm).
+    `dag` is a mapping from a node to its precedents,
+    i.e. the nodes it depends on. E.g. dag={1: [2, 3]} means
+    that node 1 depends on nodes 2 and 3. Hence [2, 3, 1] or
+    [3, 2, 1] would be the valid topological sortings here.
+
+    >>> d1 = {1: [2, 3], 2: [3]}
+    >>> list(topological_sort(d1))
+    [3, 2, 1]
+    >>> d2 = {"a": ["b", "c"], "b": ["c", "d"], "c": ["d"]}
+    >>> list(topological_sort(d2))
+    "dcba".split()
+    """
+    return []
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
