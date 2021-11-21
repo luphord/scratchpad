95abb274c256ed6d5daad8cd6892d6d063f9dd06
luphord <luphord@protonmail.com>
2021-11-21 16:51:58 +0100

    decrease zoom speed

--- julia-images/mandelbrot_video.jl
+++ julia-images/mandelbrot_video.jl
@@ -24,18 +24,21 @@ end
 let
     local center = -0.743643887037158704752191506114774 + 0.131825904205311970493132056385139im
     local initialwidth = 3.5
-    local nframes = 10
-    encoder_options = (crf=23, preset="medium")
-    framerate=25
-    frame = function(i)
-        width = initialwidth * (1 - (i-1)/nframes) 
-        mandelbrot(400, 300, maxiter = 20, threshold = 50, center = center, width = width) do x
+    local finalwidth = 1e-4
+    local nframes = 100
+    local shrinkrate = 1e-2
+    encoder_options = (crf = 23, preset = "medium")
+    framerate = 25
+    frame = function (i)
+        width = finalwidth + (initialwidth - finalwidth) * (1 - shrinkrate)^(i - 1)
+        mandelbrot(1920, 1080, maxiter = 50, threshold = 100, center = center, width = width) do x
             RGB{N0f8}((1 - cos(x * 11π)) / 2, (1 - cos(x * 2π)) / 2, (1 - cos(x * 7π)) / 2)
         end
     end
-    open_video_out("mandelbrot.mp4", frame(1), framerate=framerate, encoder_options=encoder_options) do writer
-        for i in 2:nframes
+    open_video_out("mandelbrot.mp4", frame(1), framerate = framerate, encoder_options = encoder_options) do writer
+        for i = 2:nframes
             write(writer, frame(i))
+            println(i)
         end
     end
 end
