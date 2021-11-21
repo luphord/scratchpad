0f9c3153ca9b71cf50c238e379cf99c71fb4210c
luphord <luphord@protonmail.com>
2021-11-21 15:03:56 +0100

    save a mandelbrot video

--- julia-images/mandelbrot_video.jl
+++ julia-images/mandelbrot_video.jl
@@ -1,7 +1,7 @@
 using Images, VideoIO
 
 function mandelbrot(colorize::Function, width_pixels::Int, height_pixels::Int; maxiter = 50, threshold = 100, center = -0.63 + 0im, width = 3.2)
-    img = RGB{Float32}.(colorize.(zeros(height_pixels, width_pixels)))
+    img = RGB{N0f8}.(colorize.(zeros(height_pixels, width_pixels)))
     rows, cols = size(img)
     height = width * rows / cols
     upperleft = center - width / 2 + height / 2 * im
@@ -25,11 +25,17 @@ let
     local center = -0.743643887037158704752191506114774 + 0.131825904205311970493132056385139im
     local initialwidth = 3.5
     local nframes = 10
-    for i in 1:nframes
+    encoder_options = (crf=23, preset="medium")
+    framerate=25
+    frame = function(i)
         width = initialwidth * (1 - (i-1)/nframes) 
-        img = mandelbrot(400, 300, maxiter = 20, threshold = 50, center = center, width = width) do x
-            RGB(1 - cos(x * 11π), 1 - cos(x * 2π), 1 - cos(x * 7π))
+        mandelbrot(400, 300, maxiter = 20, threshold = 50, center = center, width = width) do x
+            RGB{N0f8}((1 - cos(x * 11π)) / 2, (1 - cos(x * 2π)) / 2, (1 - cos(x * 7π)) / 2)
         end
-        display(img)
+    end
+    open_video_out("mandelbrot.mp4", frame(1), framerate=framerate, encoder_options=encoder_options) do writer
+        for i in 2:nframes
+            write(writer, frame(i))
+        end
     end
 end
