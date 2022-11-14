9ebba14294e4667aa4b9160a65ca871f45e5fd23
luphord <luphord@protonmail.com>
2022-11-14 20:18:19 +0100

    include htmx

--- structured-wiki/website.py
+++ structured-wiki/website.py
@@ -3,10 +3,13 @@ from bottle import route, static_file, run
 from htmltags import *
 
 water_css_path = "/static/css/water.css"
+htmx_path = "/static/js/htmx.min.js"
 
 water_css = link(
     rel="stylesheet", href=water_css_path
 )
+
+htmx_src = script(src=htmx_path)
 
 def home_page(greeting):
     return html(
@@ -19,12 +22,17 @@ def home_page(greeting):
             ),
             p(greeting),
             div('<script>alert("evil!")</script>', id="mydiv"),
+            htmx_src
         ),
     )
 
 @route(water_css_path)
 def watercss():
     return static_file("water.css", root=Path(__file__).parent / "assets/css")
+
+@route(htmx_path)
+def htmx():
+    return static_file("htmx.min.js", root=Path(__file__).parent / "assets/js")
 
 @route('/hello/<name>')
 def hello(name):
