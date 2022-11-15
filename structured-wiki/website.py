9461c874361b927ed93177db72994108960aeca3
luphord <luphord@protonmail.com>
2022-11-15 21:25:09 +0100

    replace water.css by bulma.css

--- structured-wiki/website.py
+++ structured-wiki/website.py
@@ -2,37 +2,41 @@ from pathlib import Path
 from bottle import route, static_file, run
 from htmltags import *
 
-water_css_path = "/static/css/water.css"
+bulma_css_path = "/static/css/bulma.min.css"
 htmx_path = "/static/js/htmx.min.js"
 
-water_css = link(
-    rel="stylesheet", href=water_css_path
+bulma_css = link(
+    rel="stylesheet", href=bulma_css_path
 )
 
 htmx_src = script(src=htmx_path)
 
 def replaceable_button(url="/removed", content="Click me to remove"):
-    return button(content, hx_trigger="click", hx_swap="outerHTML", hx_get=url)
+    return button(content, hx_trigger="click", hx_swap="outerHTML", hx_get=url, class_="button is-primary")
 
 def home_page(greeting):
     return html(
-        head(title("A Test of HTML generation"), water_css),
+        head(title("A Test of HTML generation"), bulma_css),
         body(
-            h1("A Test of HTML generation"),
-            details(
-                summary("What could be down below?"),
-                p("It's just leeeeeeeeeeeeeeeeeeeeeeeengthy text"),
-            ),
-            p(greeting),
-            replaceable_button(),
-            div('<script>alert("evil!")</script>', id="mydiv"),
-            htmx_src
+            main(
+                h1("A Test of HTML generation", class_="title"),
+                details(
+                    summary("What could be down below?"),
+                    p("It's just leeeeeeeeeeeeeeeeeeeeeeeengthy text"),
+                    class_="box"
+                ),
+                p(greeting, class_="box"),
+                replaceable_button(),
+                div('<script>alert("evil!")</script>', id="mydiv", class_="box"),
+                htmx_src,
+                class_="container"
+            )
         ),
     )
 
-@route(water_css_path)
-def watercss():
-    return static_file("water.css", root=Path(__file__).parent / "assets/css")
+@route(bulma_css_path)
+def bulmacss():
+    return static_file("bulma.min.css", root=Path(__file__).parent / "assets/css")
 
 @route(htmx_path)
 def htmx():
@@ -44,6 +48,6 @@ def hello(name):
 
 @route('/removed')
 def hello():
-    return str(p("You got replaced!"))
+    return str(p("You got replaced!", class_="notification"))
 
 run(host='localhost', port=8080, debug=True, reloader=True)
