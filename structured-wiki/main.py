4b0bc0900a57e0dc457640dfc2a376d2c2ab9c4c
luphord <luphord@protonmail.com>
2022-11-22 07:36:15 +0100

    reformat with black

--- structured-wiki/main.py
+++ structured-wiki/main.py
@@ -8,18 +8,30 @@ with open("database_setup.sql", "r") as f:
     c.executescript(f.read())
 c.commit()
 
+
 def create_view(view_name):
     c.execute("insert or ignore into views values (?)", (view_name,))
 
+
 def get_view_id(view_name):
-    return c.execute("select rowid from views where name=?", (view_name.lower(),)).fetchone()[0]
+    return c.execute(
+        "select rowid from views where name=?", (view_name.lower(),)
+    ).fetchone()[0]
+
 
 def save(object_id, view, data):
     view_id = get_view_id(view)
-    c.execute("insert into objects values (?, ?, ?, ?);", (object_id, int(time.time() * 1000), view_id, json.dumps(data)))
+    c.execute(
+        "insert into objects values (?, ?, ?, ?);",
+        (object_id, int(time.time() * 1000), view_id, json.dumps(data)),
+    )
+
 
 def get_latest(object_id):
-    return c.execute("select * from objects where object_id=? order by version desc;", (object_id,)).fetchone()
+    return c.execute(
+        "select * from objects where object_id=? order by version desc;", (object_id,)
+    ).fetchone()
+
 
 if __name__ == "__main__":
     print(get_latest(3))
--- structured-wiki/website.py
+++ structured-wiki/website.py
@@ -5,32 +5,30 @@ from htmltags import *
 bulma_css_path = "/static/css/bulma.min.css"
 htmx_path = "/static/js/htmx.min.js"
 
-bulma_css = link(
-    rel="stylesheet", href=bulma_css_path
-)
+bulma_css = link(rel="stylesheet", href=bulma_css_path)
 
 htmx_src = script(src=htmx_path)
 
+
 def navbar(brand_content, items):
-    brand = div(
-        a(
-            brand_content,
-            href="/",
-            class_="navbar-item"),
-        _class="navbar-brand")
+    brand = div(a(brand_content, href="/", class_="navbar-item"), _class="navbar-brand")
     links = div(
-        div(
-            *[a(item, class_="navbar-item") for item in items],
-            class_="navbar-start"),
+        div(*[a(item, class_="navbar-item") for item in items], class_="navbar-start"),
         div(a("Login", class_="navbar-item"), class_="navbar-end"),
-        _class="navbar-menu")
-    return nav(
-        brand,
-        links,
-        class_="navbar is-fixed-top is-info")
+        _class="navbar-menu",
+    )
+    return nav(brand, links, class_="navbar is-fixed-top is-info")
+
 
 def replaceable_button(url="/removed", content="Click me to remove"):
-    return button(content, hx_trigger="click", hx_swap="outerHTML", hx_get=url, class_="button is-info")
+    return button(
+        content,
+        hx_trigger="click",
+        hx_swap="outerHTML",
+        hx_get=url,
+        class_="button is-info",
+    )
+
 
 def home_page(greeting):
     return html(
@@ -42,31 +40,36 @@ def home_page(greeting):
                 details(
                     summary("What could be down below?"),
                     p("It's just leeeeeeeeeeeeeeeeeeeeeeeengthy text"),
-                    class_="box"
+                    class_="box",
                 ),
                 p(greeting, class_="box"),
                 replaceable_button(),
                 div('<script>alert("evil!")</script>', id="mydiv", class_="box"),
                 htmx_src,
-                class_="container"
-            )
+                class_="container",
+            ),
         ),
     )
+
 
 @route(bulma_css_path)
 def bulmacss():
     return static_file("bulma.min.css", root=Path(__file__).parent / "assets/css")
 
+
 @route(htmx_path)
 def htmx():
     return static_file("htmx.min.js", root=Path(__file__).parent / "assets/js")
 
-@route('/hello/<name>')
+
+@route("/hello/<name>")
 def hello(name):
     return str(home_page(f"hi {name}!"))
 
-@route('/removed')
+
+@route("/removed")
 def hello():
     return str(p("You got replaced!", class_="notification"))
 
-run(host='localhost', port=8080, debug=True, reloader=True)
+
+run(host="localhost", port=8080, debug=True, reloader=True)
