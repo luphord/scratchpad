1521006870a4621c6dbdded6f30fbbfa1237dfae
luphord <luphord@protonmail.com>
2022-12-08 07:55:13 +0100

    add route spec for type definitions

--- structured-wiki/routes.py
+++ structured-wiki/routes.py
@@ -8,6 +8,15 @@ GET /edit/page  # get editing mask for a new page (no ID yet)
 POST /edit/page  # submit form to create a new page
 
 
+GET /list/type  # list all type definitions
+
+GET /edit/type/mytype  # get editing mask (form) for type definition of type mytype
+PUT /edit/type/mytype  # submit form to update type definition of type mytype
+
+GET /edit/type  # get editing mask (form) for a new type (no name yet)
+POST /edit/type  # submit form to create a new type
+
+
 GET /list/object/mytype  # list all objects of type mytype
 
 GET /show/object/mytype/111  # show object with ID 111 as mytype
